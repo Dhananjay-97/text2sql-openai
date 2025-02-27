72bcc26d9c32ae6a678ac977a977ba98d0589e6f
Dhananjay Lokhande <125077594+Dhananjay-97@users.noreply.github.com>
2025-02-27 11:20:35 +0530

    Update install_dependencies.py

--- scripts/1_session-install-deps/install_dependencies.py
+++ scripts/1_session-install-deps/install_dependencies.py
@@ -1,2 +1,2 @@
 !pip install --upgrade pip
-!pip install --no-cache-dir --log 1_session-install-deps/pip-req.log -r 1_session-install-deps/requirements.txt
+!pip install --no-cache-dir --log 1_session-install-deps/pip-req.log -r scriprs/1_session-install-deps/requirements.txt
