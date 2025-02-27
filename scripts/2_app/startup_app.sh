12c4a5e2da44bb243d28e108c05fd2f2d7b30bad
Dhananjay Lokhande <125077594+Dhananjay-97@users.noreply.github.com>
2025-02-27 14:47:31 +0530

    Update startup_app.sh

--- scripts/2_app/startup_app.sh
+++ scripts/2_app/startup_app.sh
@@ -15,7 +15,6 @@ done
 trap cleanup EXIT
 
 export RAG_DATABASES_DIR=$(pwd)/databases
-# export LLM_SERVICE_URL="http://localhost:8081"
 export API_URL="http://localhost:8080"
 
 # Start FastAPI backend
@@ -24,11 +23,6 @@ uvicorn backend:app --reload --host 0.0.0.0 --port 8080 > backend.log 2>&1 &
 # Start Streamlit frontend
 streamlit run frontend.py > frontend.log 2>&1 &
 
-# wait for the python backend to be ready
-# while ! curl --output /dev/null --silent --fail http://localhost:8080/docs; do
-#     echo "Waiting for the Python backend to be ready..."
-#     sleep 4
-# done
 while ! curl --output /dev/null --silent --fail http://localhost:8080/docs; do
     echo "Waiting for the Python backend to be ready..."
     sleep 4
